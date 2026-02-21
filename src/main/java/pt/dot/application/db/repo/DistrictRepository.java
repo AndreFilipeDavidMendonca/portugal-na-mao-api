76a943207ad34e2aa1017614594014925df8dc05
andre.mendonca <andre.mendonca@polarising.com>
2026-02-21 02:47:33 +0100

    Clean code

--- src/main/java/pt/dot/application/db/repo/DistrictRepository.java
+++ src/main/java/pt/dot/application/db/repo/DistrictRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import pt.dot.application.db.entity.District;
 
-import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,12 +16,12 @@ public interface DistrictRepository extends JpaRepository<District, Long> {
     Optional<District> findByNamePtIgnoreCase(String namePt);
 
     @Query(value = """
-  select *
-  from district d
-  where unaccent(lower(coalesce(d.name_pt, d.name))) like concat('%%', unaccent(lower(:q)), '%%')
-     or unaccent(lower(coalesce(d.name, ''))) like concat('%%', unaccent(lower(:q)), '%%')
-  order by coalesce(d.name_pt, d.name) asc
-  limit :limit
-""", nativeQuery = true)
+      select *
+      from district d
+      where unaccent(lower(coalesce(d.name_pt, d.name))) like concat('%%', unaccent(lower(:q)), '%%')
+         or unaccent(lower(coalesce(d.name, ''))) like concat('%%', unaccent(lower(:q)), '%%')
+      order by coalesce(d.name_pt, d.name) asc
+      limit :limit
+    """, nativeQuery = true)
     List<District> searchByName(@Param("q") String q, @Param("limit") int limit);
 }
