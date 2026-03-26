5a2389c9ac5726c559ea5dbb94bddfe328f22b04
André Mendonça <andredmendonca89@gmail.com>
2026-03-26 02:11:53 +0000

    Clean patch

--- src/main/resources/db/migration/V8__make_chat_message_body_nullable.sql
+++ src/main/resources/db/migration/V8__make_chat_message_body_nullable.sql
@@ -1,4 +1,2 @@
-cat > src/main/resources/db/migration/V8__make_chat_message_body_nullable.sql <<'SQL'
 ALTER TABLE chat_message
-    ALTER COLUMN body DROP NOT NULL;
-SQL
+ALTER COLUMN body DROP NOT NULL;
